966880b93d7e8f668894fcabd332b3bdcc18c6a0
Muhammet Soysal <msoysal92@gmail.com>
2019-02-11 19:50:04 +0300

    License model updated

--- src/main/java/com/msoft/licenses/model/License.java
+++ src/main/java/com/msoft/licenses/model/License.java
@@ -1,15 +1,41 @@
 package com.msoft.licenses.model;
 
-import lombok.Builder;
-import lombok.Data;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-@Builder
-@Data
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "licenses")
 public class License {
 
-	private String id;
+	@Id
+	@Column(name = "license_id", nullable = false)
+	private String licenseId;
+
+	@Column(name = "organization_id", nullable = false)
 	private String organizationId;
+
+	@Column(name = "product_name", nullable = false)
 	private String productName;
+
+	@Column(name = "license_type", nullable = false)
 	private String licenseType;
-	
+
+	@Column(name = "license_max", nullable = false)
+	private Integer licenseMax;
+
+	@Column(name = "license_allocated", nullable = false)
+	private Integer licenseAllocated;
+
+	@Column(name = "comment")
+	private String comment;
+
 }
